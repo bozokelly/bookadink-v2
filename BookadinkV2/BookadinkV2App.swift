2d900de1bf4da6614552ef3f5525b6b6fd99a80a
Brayden <bozokelly@gmail.com>
2026-02-23 22:00:07 +0800

    Initial Commit

---
+++ BookadinkV2/BookadinkV2App.swift
@@ -0,0 +1,17 @@
+//
+//  BookadinkV2App.swift
+//  BookadinkV2
+//
+//  Created by Brayden Kelly on 23/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookadinkV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookadinkV2Tests/BookadinkV2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BookadinkV2Tests.swift
+//  BookadinkV2Tests
+//
+//  Created by Brayden Kelly on 23/2/2026.
+//
+
+import Testing
+@testable import BookadinkV2
+
+struct BookadinkV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
